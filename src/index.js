07f2ad7a80601e12c60753f725e3ac359d92852f
Muhammad Rehman Rafi <153813023+M-Rehmans@users.noreply.github.com>
2026-09-06 17:55:55 +0500

    ewer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import "./styles.css";
+import "./components/Login.css";
+import "./components/Signup.css";
 import App from "./components/App";
 
 const rootElement = document.getElementById("root");
