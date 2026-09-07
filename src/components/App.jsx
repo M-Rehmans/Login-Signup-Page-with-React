48dc6f32272137052d50baec8ada647828f8a19a
Muhammad Rehman Rafi <153813023+M-Rehmans@users.noreply.github.com>
2026-09-07 20:28:31 +0500

    Add the function for toggle the form login/signup by using the useState function in react js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,14 @@ import React, {useState} from "react";
 import Form from "./Form";
 
 // var isSignup = false;
-var [isSignup, formToggle] = useState(false);
 
 function App() {
+  var [isSignup, formShift] = useState(false);
+  function formToggle(e) {
+    e.preventDefault();
+    formShift(!isSignup);
+  }
+
   return (
     <main className="auth-container">
       <section
@@ -15,7 +20,7 @@ function App() {
           <h1>{isSignup ? "Create Account" : "Welcome Back"}</h1>
           <p>{isSignup ? "Sign up to get started" : "Login to your account"}</p>
         </div>
-        <Form isSignup={isSignup} />
+        <Form formType={isSignup} changeForm={formToggle}  />
       </section>
     </main>
   );
